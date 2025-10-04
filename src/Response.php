b9627d6335dd3e3217045082f575f59cbab58ba9
Manh Huy Vo <manhhuyvo.coding@gmail.com>
2025-10-04 16:06:38 +1000

    test class with autoload

---
+++ src/Response.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ManhHuyVo\ActionResponse;
+
+require_once 'vendor/autoload.php';
+
+class Response
+{
+    public function test()
+    {
+        echo "test";
+    }
+}
